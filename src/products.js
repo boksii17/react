ea572c0ce8c993af2cd4a658da2626a88a1ddfd7
Bojana Novakovic <novakovicc17@gmail.com>
2023-03-18 23:29:58 +0100

    krerian niz proizvoda i dodate slike

---
+++ src/products.js
@@ -0,0 +1,47 @@
+import product1 from './pictures/iphone.jpg';
+import product2 from './pictures/samsunggalaxya23.jpg';
+import product3 from './pictures/hplaptop.jpg';
+import product4 from './pictures/appleairpods.jpg';
+import product5 from './pictures/samsungearbuds.jpg';
+import product6 from './pictures/nikonkamera.jpg';
+
+
+export const PRODUCTS = [
+   {
+    id: 1, 
+    productName: "IPhone",
+    price: 700.0,
+    productImage: product1,
+   },
+   {
+    id: 2, 
+    productName: "Samsung Galaxy A23",
+    price: 260.0,
+    productImage: product2,
+   },
+   {
+    id: 3, 
+    productName: "HP Laptop",
+    price: 1100.0,
+    productImage: product3,
+   },
+   {
+    id: 4, 
+    productName: "Apple Airpods",
+    price: 300.0,
+    productImage: product4,
+   },
+   {
+    id: 5, 
+    productName: "Samsung Earbuds",
+    price: 200.0,
+    productImage: product5,
+   },
+   {
+    id: 6, 
+    productName: "Nikon Kamera",
+    price: 950.0,
+    productImage: product6,
+   },
+
+];

4a3ead3a9d1520bc6d89466b68264471fe6537e2
Bojana Novakovic <novakovicc17@gmail.com>
2023-03-29 12:20:29 +0200

    dodato jos proizvoda u shop-u

--- src/products.js
+++ src/products.js
@@ -4,12 +4,17 @@ import product3 from './pictures/hplaptop.jpg';
 import product4 from './pictures/appleairpods.jpg';
 import product5 from './pictures/samsungearbuds.jpg';
 import product6 from './pictures/nikonkamera.jpg';
+import product7 from './pictures/iphone12pro.jpg' ;
+import product8 from './pictures/asus.jpg';
+import product9 from './pictures/iphone13.jpg';
+import product10 from './pictures/samsunggalaxya23.jpg';
+import product11 from './pictures/canonEOS.jpg';
 
 
 export const PRODUCTS = [
    {
     id: 1, 
-    productName: "IPhone",
+    productName: "IPhone 11",
     price: 700.0,
     productImage: product1,
    },
@@ -43,5 +48,35 @@ export const PRODUCTS = [
     price: 950.0,
     productImage: product6,
    },
+   {
+      id: 7, 
+      productName: "IPhone 12pro",
+      price: 700.0,
+      productImage: product7,
+     },
+     {
+      id: 8, 
+      productName: "ASUS laptop",
+      price: 700.0,
+      productImage: product8,
+     },
+     {
+      id: 9, 
+      productName: "Iphone 13",
+      price: 700.0,
+      productImage: product9,
+     },
+     {
+      id: 10, 
+      productName: "Samsung Galaxy 23",
+      price: 700.0,
+      productImage: product10,
+     },
+     {
+      id: 11, 
+      productName: "Canon EOS",
+      price: 700.0,
+      productImage: product11,
+     },
 
 ];
